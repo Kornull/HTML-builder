60411d0c4ec6ef17e28eac1a18659c98fb0c9162
Kornull <wolod1987kor@gmail.com>
2022-05-19 20:37:45 +0300

    02

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+
+const rl = readline.createInterface({ input, output });
+const filePath = path.join(__dirname, '../02-write-file', 'text.txt');
+const stream = fs.createWriteStream(filePath);
+
+console.log('File created, enter text please:\n');
+
+rl.on('line', (line) => {
+  if (!line.toLowerCase().includes('exit')) {
+    stream.write(`${line}\n`);
+  } else {
+    rl.close();
+  }
+});
+
+process.on('exit', () => {
+  console.log('File "text.txt" saved.\tGoodbye.');
+});
+
