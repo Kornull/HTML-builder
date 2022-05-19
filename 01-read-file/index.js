082a18cc96c3b821c973b329b89aed6d447a0a18
Kornull <wolod1987kor@gmail.com>
2022-05-19 14:13:23 +0300

    01

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const fs = require('fs');
+
+const filename = path.join(__dirname, '../01-read-file', 'text.txt');
+
+const stream = new fs.ReadStream(filename, 'utf-8');
+
+stream.on('readable',(error) =>{
+  if(error) throw error;
+  const data = stream.read()
+  if(data !== null) console.log(data)
+});
