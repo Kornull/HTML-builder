834c630c0000605bced2fa3a15e06914303886ce
Kornull <wolod1987kor@gmail.com>
2022-05-19 14:32:51 +0300

    01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,8 +5,8 @@ const filename = path.join(__dirname, '../01-read-file', 'text.txt');
 
 const stream = new fs.ReadStream(filename, 'utf-8');
 
-stream.on('readable',(error) =>{
-  if(error) throw error;
-  const data = stream.read()
-  if(data !== null) console.log(data)
+stream.on('readable', (error) => {
+  if (error) throw error;
+  const data = stream.read();
+  if (data !== null) console.log(data);
 });
