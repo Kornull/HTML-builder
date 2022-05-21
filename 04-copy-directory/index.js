16dc23c10ca1d8b9c6687a2b8ac34b2b8a976fb4
Kornull <wolod1987kor@gmail.com>
2022-05-21 16:01:40 +0300

    04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+let fs = require('fs');
+const path = require('path');
+const { COPYFILE_EXCL } = fs.constants;
 
+const directory = path.join(__dirname, 'files');
+const copyDirectory = path.join(__dirname, `files-copy-${Math.floor(Math.random() * 100)}`);
+
+fs.mkdir(copyDirectory, { recursive: true }, err => {
+  if (err) throw err;
+});
+
+fs.readdir(directory, (error, files) => {
+  if (error) throw error;
+  files.forEach(f => {
+    const fileDir = path.join(directory, f);
+    const fileCopyDir = path.join(copyDirectory, f);
+    fs.copyFile(fileDir, fileCopyDir, COPYFILE_EXCL, error => {
+      if (error) throw error;
+      console.log('File copied');
+    });
+  });
+});
