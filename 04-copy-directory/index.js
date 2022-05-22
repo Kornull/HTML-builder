643ddb24aff25699ec78b3fc551cdaae325ed531
Kornull <wolod1987kor@gmail.com>
2022-05-22 22:22:30 +0300

    feat:add 04-task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const directory = path.join(__dirname, 'files');
 const copyDirectory = path.join(__dirname, 'files-copy');
 
+
 fs.mkdir(copyDirectory, { recursive: true }, err => {
   if (err) throw err;
 });
