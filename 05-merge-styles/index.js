8f676a33124144680afa08bdb60e7020f89cae20
Kornull <wolod1987kor@gmail.com>
2022-05-22 22:51:50 +0300

    fix: 05-task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -8,18 +8,23 @@ const styleFile = path.join(__dirname, 'styles');
 
 
 (async () => {
-  fs.readdir(styleFile, (err, f) => {
-    if(err) throw err;
-    f.forEach(file => {
-      if (file.split('.').pop() === 'css') {
-        let data = '';
-        const allPath = path.join(styleFile, file);
-        const stream = fs.createReadStream(allPath, 'utf-8');
-        stream.on('data', (chunk) => {
-          data += chunk;
-          streamWrite.write(`${data}\n`);
-        });
-      }
+  fs.readdir(styleFile, (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+      fs.stat(`${styleFile}/${file}`, (error, f) => {
+        if (error) throw error;
+        if (f.isFile()) {
+          if (file.split('.').pop() === 'css') {
+            let data = '';
+            const allPath = path.join(styleFile, file);
+            const stream = fs.createReadStream(allPath, 'utf-8');
+            stream.on('data', (chunk) => {
+              data += chunk;
+              streamWrite.write(`${data}\n`);
+            });
+          }
+        }
+      });
     });
   });
 })();
