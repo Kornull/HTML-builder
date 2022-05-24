1e54c18805681b022e024e39359b34250488875a
Kornull <wolod1987kor@gmail.com>
2022-05-24 04:23:08 +0300

    feat:add 06-task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,6 +1,5 @@
 let fs = require('fs');
 const path = require('path');
-
 const directory = path.join(__dirname, 'project-dist');
 
 fs.mkdir(directory, { recursive: true }, err => {
@@ -14,90 +13,35 @@ const streamWrite = fs.createWriteStream(filePath);
 const indexHtmlPath = path.join(__dirname, 'project-dist', 'index.html');
 const streamWriteIndexHtml = fs.createWriteStream(indexHtmlPath);
 
-// const htmlFiles = path.join(__dirname, 'components');
+const htmlFiles = path.join(__dirname, 'components');
 
-const indexHtmlFile = path.join(__dirname);
-console.log(indexHtmlFile);
-// const readline = require('readline');
+const readline = require('readline');
 
 
 
 
-
-
-
-
-
-
-async function copyFile(pathFile, streamPath, fileName) {
-  fs.readdir(pathFile, (err, files) => {
+(async () =>{
+  fs.readdir(styleFile, (err, files) => {
     const a = sortedFiles(files);
     if (err) throw err;
     a.forEach(file => {
-      fs.stat(`${pathFile}/${file}`, (error, f) => {
+      fs.stat(`${styleFile}/${file}`, (error, f) => {
         if (error) throw error;
-        if (f.isFile() && file.split('.').pop() === fileName) {
+        if (f.isFile() && file.split('.').pop() === 'css') {
           let data = '';
-          const allPath = path.join(pathFile, file);
+          const allPath = path.join(styleFile, file);
           const stream = fs.createReadStream(allPath, 'utf-8');
           stream.on('data', (chunk) => {
             data += chunk;
-            streamPath.write(`${data}\n`);
+            streamWrite.write(`${data}\n`);
           });
         }
       });
     });
   });
-
-}
-
+})();
 
 
-async function readIndexFile() {
-  const file = path.join(__dirname, 'project-dist');
-  fs.readdir(file, (err, files) => {
-    console.log(files[0]);
-    const indexFile = path.join(file,);
-    console.log(indexFile);
-    const stream = fs.readFile(files[0], 'utf-8');
-    // console.log(stream);
-    read(stream);
-  });
-
-
-
-
-
-
-  //   // console.log(files);
-  //   // const a = sortedFiles(files);
-  //   // console.log(a);
-  //   if (err) throw err;
-
-  //   files.forEach(file => {
-  //     fs.stat(`${styleFile}/${file}`, (error, f) => {
-  //       if (error) throw error;
-  //       if (f.isFile()) {
-  //         if (file.split('.').pop() === 'css') {
-  //           let data = '';
-  //           const allPath = path.join(styleFile, file);
-  //           const stream = fs.createReadStream(allPath, 'utf-8');
-  //           stream.on('data', (chunk) => {
-  //             data += chunk;
-  //             streamWrite.write(`${data}\n`);
-  //           });
-  //         }
-  //       }
-  //     });
-  //   });
-  //   });
-}
-function read(stream) {
-  let data = '';
-  stream.on('data', chunk => data += chunk);
-  stream.on('end', () => console.log('kk', data));
-  stream.on('error', error => console.log(error.message));
-}
 
 function sortedFiles(files) {
   const arr = [];
@@ -145,10 +89,36 @@ async function createAssets() {
   });
 }
 
+const objHtmlFiles = async function () {
+  const objHtml = {};
+  const files = await fs.promises.readdir(htmlFiles, { withFileTypes: true });
+  for (let i of files) {
+    const pathHtml = path.join(htmlFiles, i.name);
+    const fileText = await fs.promises.readFile(pathHtml, 'utf-8');
+    objHtml[i.name.split('.')[0]] = fileText.toString();
+  }
+  return objHtml;
+};
+
+async function readIndex() {
+  const filesText = await objHtmlFiles();
+  const dir = path.join(__dirname, 'template.html');
+
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(dir),
+  });
+  readInterface.on('line', function (line) {
+    for (let a of Object.keys(filesText)) {
+      if (line.includes(`{{${a}}}`)) {
+        line = filesText[a];
+      }
+    }
+    streamWriteIndexHtml.write(`${line}\n`);
+  });
+}
+
 
 (async () => {
-  await copyFile(indexHtmlFile, streamWriteIndexHtml, 'html');
-  await copyFile(styleFile, streamWrite, 'css');
-  createAssets();
-  // readIndexFile();
+  await createAssets();
+  readIndex();
 })();
